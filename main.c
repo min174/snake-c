2d0e41add953a602666fd156cef1a1265c7546c8
min174 <mingaile.michejeva@ucdconnect.ie>
2025-10-08 20:31:36 +0100

    i drew the board!🥳

--- main.c
+++ main.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    printf("Hello World");
+const int HEIGHT=10;
+const int WIDTH=33;
+
+void draw_board();
+
+int main() {
+    draw_board();
+    return 0;
+}
+
+void draw_board() {
+    int i;
+
+    for (i = 0; i < WIDTH; i++) {
+        printf("#");
+    }
+
+    printf("\n");
+
+    for (i=0;i<HEIGHT;i++) {
+        printf("#\t\t\t\t#\n");
+    }
+
+    for (i = 0; i < WIDTH; i++) {
+        printf("#");
+    }
 }
