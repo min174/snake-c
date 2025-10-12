6ea292887d5fd8e17a5f1a14d47578f6e48544fd
min174 <mingaile.michejeva@ucdconnect.ie>
2025-10-12 23:56:14 +0100

    added some comments

--- main.c
+++ main.c
@@ -4,18 +4,20 @@
 #include <conio.h>
 #include <windows.h>
 
+//board will always stay the same
 const int HEIGHT=9;
 const int WIDTH=33;
 
+//position of the snakes head will be shared with the whole program
 int snake_X;
 int snake_Y;
 
+//position of the fruit will be shared with the whole program
 int fruit_X;
 int fruit_Y;
 
 void starting_position();
 void draw_board();
-
 
 int main() {
 
@@ -25,7 +27,6 @@ int main() {
         draw_board();
         printf("Use WASD to move the snake: ");
         Sleep(300);
-        printf("\n");
     }
 
     return 0;
@@ -34,14 +35,17 @@ int main() {
 void starting_position() {
     srand(time(NULL));
 
+    //places snakes head in the centre of the board
     snake_X = WIDTH/2;
     snake_Y = HEIGHT/2;
 
+    //generates random X coordinate, if its zero, randomise again
     fruit_X = rand()%WIDTH;
     while (fruit_X == 0) {
         fruit_X = rand()%WIDTH;
     }
 
+    //generates random Y coordinate, if its zero, randomise again
     fruit_Y = rand()%HEIGHT;
     while (fruit_Y == 0) {
         fruit_Y = rand()%HEIGHT;
@@ -81,3 +85,4 @@ void draw_board() {
 
     printf("\n");
 }
+
